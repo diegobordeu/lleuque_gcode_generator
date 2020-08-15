6f81e6a7b00c8a3ea310ea30774a9e6d7c0305f6
Diego Bordeu <diego@fingo.cl>
2020-08-14 20:37:27 -0400

    gcode.py

---
+++ gcode.py
@@ -0,0 +1,41 @@
+
+
+
+
+class GCode(object):
+    
+
+    def __init__(self, x_steps=49, y_steps=19, pause=1, speed=50):
+        self.x_steps = x_steps
+        self.y_steps = y_steps
+        self.pause = pause
+        self.speed = speed
+        self.file = open("grilla.gcode", "w")
+        self.x_start = 1
+        self.y_start = 1
+
+    def move_to(self, x, y, speed):
+        self.file.write(f"G90 G1 X{x} Y{y} F{speed}\n")
+        self.file.write(f"G4 P{self.pause}\n")
+
+    def init_file(self):
+        self.file.write("G21 ; Set units to mm\n")
+        self.file.write("G90 ; Absolute positioning\n")
+
+    def build_file(self):
+        x_end = self.x_start + self.x_steps
+        y_end = self.y_start + self.y_steps
+        x_temp = self.x_start
+        y_temp = self.y_start
+        while x_temp < x_end:
+            if y_temp == self.y_start:
+                while y_temp < y_end:
+                    self.move_to(x_temp, y_temp, self.speed)
+                    y_temp += 1
+                self.move_to(x_temp, y_temp, self.speed)
+                x_temp += 1
+                while y_temp > self.y_start:
+                    self.move_to(x_temp, y_temp, self.speed)
+                    y_temp -= 1
+                self.move_to(x_temp, y_temp, self.speed)
+                x_temp += 1
